b2749add156577e99801d357da48f12b0ecaa775
mauriterry <cmrodriguez67@ucatolica.edu.co>
2019-11-25 23:36:23 -0500

    cambio graficas

--- app/modules/page/Views/graficas/index.php
+++ app/modules/page/Views/graficas/index.php
@@ -1,75 +1,43 @@
 <div class="titulo-internas1" style="background-image:url(/skins/page/images/fondo1.jpg)">
     <div align="center"><h2>Graficas</h2></div>
 </div>
-<div id="charts">
-		<div class="wrapper"><canvas id="chart-0"></canvas></div>
-	</div>
-
-	<script>
-		var SAMPLE_INFO = {
-			group: 'Charts',
-			name: 'Line',
-		};
-	</script>
-
-	<script id="script-construct">
-		var chart = new Chart('chart-0', {
-			type: 'line',
-			data: {
-				labels: labels,
-				datasets: [{
-					backgroundColor: Samples.color(0),
-					borderColor: Samples.color(0),
-					data: Samples.numbers({
-						count: DATA_COUNT,
-						min: 0,
-						max: 100
-					}),
-					datalabels: {
-						align: 'start',
-						anchor: 'start'
-					}
-				}, {
-					backgroundColor: Samples.color(1),
-					borderColor: Samples.color(1),
-					data: Samples.numbers({
-						count: DATA_COUNT,
-						min: 0,
-						max: 100
-					})
-				}, {
-					backgroundColor: Samples.color(2),
-					borderColor: Samples.color(2),
-					data: Samples.numbers({
-						count: DATA_COUNT,
-						min: 0,
-						max: 100
-					}),
-					datalabels: {
-						align: 'end',
-						anchor: 'end'
-					}
-				}]
-			},
-			options: {
-				plugins: {
-					datalabels: {
-						backgroundColor: function(context) {
-							return context.dataset.backgroundColor;
-						},
-						borderRadius: 4,
-						color: 'white',
-						font: {
-							weight: 'bold'
-						},
-						formatter: Math.round
-					}
-				},
-				scales: {
-					yAxes: [{
-						stacked: true
-					}]
-				}
-			}
-		});
-	</script>
+<canvas id="myChart" width="50" height="50"></canvas>
+<script>
+	var ctx = document.getElementById('myChart');
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: ['0', '1', '2', '3', '4', '5', '6'],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [10, 20, 50, 60, 100, 200, 300],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255, 99, 132, 1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero: true
+	                }
+	            }]
+	        }
+	    }
+	});
+</script>
