511407a48d25a3a623845a6758b04e5f775a2253
mauriterry <cmrodriguez67@ucatolica.edu.co>
2019-11-26 20:53:14 -0500

    cambios carrousel

--- app/modules/page/Views/template/carrousel.php
+++ app/modules/page/Views/template/carrousel.php
@@ -1,21 +1,18 @@
 <div class="padding-content" >
     <div class="container">
-        <h2 class="titulo2-principal">Nuestras Sedes</h2>
-        <div>
-            <div id='carouselsect'>
-                <div class='left_scroll'><i class="fas fa-chevron-circle-left"></i></div>
-                <div class="carousel_inner">
-                    <ul>
-						<?php $colorfondo = $columna->contenido_fondo_color; ?>
-						<?php foreach ($carrousel as $key => $contenido): ?>
-							<li style="width: 343.333px;">
-								<?php include($disenio); ?>
-							</li>
-						<?php endforeach ?>
-                    </ul>
-                </div>
-                <div class='right_scroll'><i class="fas fa-chevron-circle-right"></i></div>
+        <div id='carouselsect'>
+            <div class='left_scroll'><i class="fas fa-chevron-circle-left"></i></div>
+            <div class="carousel_inner">
+                <ul>
+					<?php $colorfondo = $columna->contenido_fondo_color; ?>
+					<?php foreach ($carrousel as $key => $contenido): ?>
+						<li style="width: 343.333px;">
+							<?php include($disenio); ?>
+						</li>
+					<?php endforeach ?>
+                </ul>
             </div>
+            <div class='right_scroll'><i class="fas fa-chevron-circle-right"></i></div>
         </div>
     </div>
 </div>
